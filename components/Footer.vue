6e17457c6037105de949fcc43ef3e7b1138aa552
chenwenfeng <w712915@gmail.com>
2021-10-05 00:24:38 +0900

    header、footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -30,7 +30,7 @@
                     Products
                   </nuxt-link>
                 </li>
-                <li>
+                <!-- <li>
                   <a
                     href="/admin"
                     target="_blank"
@@ -39,7 +39,7 @@
                   >
                     Admin
                   </a>
-                </li>
+                </li> -->
               </ul>
             </div>
             <div class="mt-12 md:mt-0">
@@ -69,7 +69,7 @@
         class="pt-8 mt-8 border-t border-gray-700 md:flex md:items-center md:justify-between"
       >
         <div class="flex space-x-6 md:order-2">
-          <a
+          <!-- <a
             href="https://twitter.com/lukeocodes"
             target="_blank"
             class="text-gray-400 hover:text-gray-300"
@@ -80,9 +80,9 @@
                 d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84"
               />
             </svg>
-          </a>
+          </a> -->
           <a
-            href="https://github.com/lukeocodes/nuxt-starter-netlify-cms"
+            href="https://github.com/w712915"
             target="_blank"
             class="text-gray-400 hover:text-gray-300"
           >
@@ -97,8 +97,9 @@
           </a>
         </div>
         <p class="mt-8 text-base leading-6 text-gray-400 md:mt-0 md:order-1">
-          &copy; 2020 Whoever | Sponsored by
-          <a href="https://netlify.com" target="_blank">Netlify</a>
+          <!-- &copy; 2020 Whoever | Sponsored by -->
+          &copy;<script>document.write(new Date().getFullYear());</script> Chin Bunhu
+          <!-- <a href="https://netlify.com" target="_blank">Netlify</a> -->
         </p>
       </div>
     </div>
